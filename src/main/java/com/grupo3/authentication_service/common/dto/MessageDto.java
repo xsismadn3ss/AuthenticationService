e1be5b808edd4968feb1a766b950a9963e7d03df
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-12 00:19:22 -0600

    Agrega clases comunes y configuración de seguridad:
    - Introduce `MessageDto` y `ErrorDto` para la transferencia de mensajes y errores, respectivamente.
    - Configura la clase `SecurityConfig` con un `PasswordEncoder` usando BCrypt.

---
+++ src/main/java/com/grupo3/authentication_service/common/dto/MessageDto.java
@@ -0,0 +1,12 @@
+package com.grupo3.authentication_service.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+    private String message;
+}
