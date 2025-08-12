a6acae2623cc1b9cceffd660c8f8b633cc1a5993
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-12 00:18:24 -0600

    Agrega prueba inicial `AutenticationServiceApplicationTests` para verificar carga del contexto de Spring Boot

---
+++ src/test/java/com/grupo3/authentication_service/AutenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
