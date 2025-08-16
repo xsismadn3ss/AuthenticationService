f6191eec45d16ec0867e71de9c073268a1774312
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-15 19:18:45 -0600

    Agrega configuración de filtro de seguridad en `SecurityConfig`:
    - Introduce un bean `SecurityFilterChain` para manejar las configuraciones de seguridad HTTP.
    - Permite todas las solicitudes entrantes mediante `permitAll`.
    - Deshabilita características como CSRF, autenticación básica y formulario de inicio de sesión.

--- src/main/java/com/grupo3/authentication_service/common/config/SecurityConfig.java
+++ src/main/java/com/grupo3/authentication_service/common/config/SecurityConfig.java
@@ -2,13 +2,29 @@ package com.grupo3.authentication_service.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("*")
+                        .permitAll()
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable());
+        return http.build();
     }
 }
