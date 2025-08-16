cd39f25ae70407cffa7ccf75bb295cd0c34d2a79
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-15 19:18:16 -0600

    Agrega la clase `LoginResponseDto` para representar la respuesta del proceso de autenticación:
    - Introduce propiedades `message`, `token` y `username`.
    - Actualiza `LoginUserDto` ajustando el formato del código.

--- src/main/java/com/grupo3/authentication_service/login/dto/LoginResponseDto.java
+++ src/main/java/com/grupo3/authentication_service/login/dto/LoginResponseDto.java
@@ -7,8 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginUserDto {
-
+public class LoginResponseDto {
+    private String message;
+    private String token;
     private String username;
-    private String password;
 }
