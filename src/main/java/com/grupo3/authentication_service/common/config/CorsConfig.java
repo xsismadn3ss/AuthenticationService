86218693d8c5464809ade1dc5c61de6006c4902f
Abraham <132122513+xsismadn3ss@users.noreply.github.com>
2025-08-21 00:36:02 -0600

    Modificar mapping para aplicar la configuración del CORS

--- src/main/java/com/grupo3/authentication_service/common/config/CorsConfig.java
+++ src/main/java/com/grupo3/authentication_service/common/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
