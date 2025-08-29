7e124ab7e53970f33fc54d5a08c46da7c4e81681
Harold Herrera <hrldrmnd@gmail.com>
2025-08-29 15:47:16 -0600

    Implementar Manejador de Exceptiones globales

---
+++ src/main/java/com/grupo3/authentication_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.grupo3.authentication_service.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+import java.time.LocalDateTime;
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", "Error de Validación");
+
+        // Detalle de errores por campo
+        Map<String, String> fieldErrors = new LinkedHashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        body.put("messages", fieldErrors);
+
+        return body;
+    }
+}
