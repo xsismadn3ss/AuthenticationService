bd834302c2217940587f095c07a7ea02248c55fc
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-10-11 23:56:12 -0600

    añadir prefijo para los endpoints

--- src/main/java/com/grupo3/authentication_service/encrypt/EncryptController.java
+++ src/main/java/com/grupo3/authentication_service/encrypt/EncryptController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/encryption")
+@RequestMapping("${app.prefix}/encryption")
 public class EncryptController {
 
     private final IEncryptService encryptService;
